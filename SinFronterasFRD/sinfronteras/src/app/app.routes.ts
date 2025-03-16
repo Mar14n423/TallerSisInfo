d78807e229121a637786db2447b32bc8b1654b40
FernandoNB06 <fernando.nogales@ucb.edu.bo>
2025-03-16 04:29:36 -0400

    Se añadieron las paginas HTML Y estilos CSS

--- SinFronterasFRD/sinfronteras/src/app/app.routes.ts
+++ SinFronterasFRD/sinfronteras/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { CrearTuCuentaComponent } from './paginas/crear-tu-cuenta/crear-tu-cuenta.component';
+import { IniciarSesionEmpresaComponent } from './paginas/iniciar-sesion-empresa/iniciar-sesion-empresa.component';
+import { IniciarSesionNormalComponent } from './paginas/iniciar-sesion-normal/iniciar-sesion-normal.component';
+import { IndexComponent } from './paginas/index/index.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: IndexComponent },  // Página principal
+  { path: 'crear-cuenta', component: CrearTuCuentaComponent },
+  { path: 'iniciar-sesion-empresa', component: IniciarSesionEmpresaComponent },
+  { path: 'iniciar-sesion-normal', component: IniciarSesionNormalComponent },
+];
---
+++ SinFronterasFRD/sinfronteras/src/app/paginas/iniciar-sesion-empresa/iniciar-sesion-empresa.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-iniciar-sesion-empresa',
+  standalone: true,
+  imports: [],
+  templateUrl: './iniciar-sesion-empresa.component.html',
+  styleUrl: './iniciar-sesion-empresa.component.scss'
+})
+export class IniciarSesionEmpresaComponent {
+
+}
