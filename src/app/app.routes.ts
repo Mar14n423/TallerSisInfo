550126ccbfc860629c9f28d258f61b589958c83c
Mar14n423 <wara.mercado@ucb.edu.bo>
2025-03-17 00:08:17 -0400

    funcionamiento de registro

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,9 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { RegisterComponent } from './components/register/register.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent }, // Página de inicio
+  { path: 'registro', component: RegisterComponent }
 ];
+
--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FooterComponent } from '../../shared/footer/footer.component';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [CommonModule, FormsModule, ReactiveFormsModule], 
+  imports: [CommonModule, FormsModule, ReactiveFormsModule, FooterComponent], 
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
 })
