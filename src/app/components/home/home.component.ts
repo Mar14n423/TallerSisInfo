70bad4aa7edfecf8c16322bdeb68c96d083df42c
Mar14n423 <wara.mercado@ucb.edu.bo>
2025-03-16 23:54:57 -0400

    integración de footer y navbar(falta detalles)

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { FooterComponent } from '../../shared/footer/footer.component';
 
 @Component({
   standalone: true,
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
-  imports: [CommonModule, RouterModule] // Importamos lo necesario
+  imports: [CommonModule, RouterModule, FooterComponent] // Importamos lo necesario
 })
 export class HomeComponent {}
 
