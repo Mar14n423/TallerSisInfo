fdfbe5fea520dd4f4e3bb70da6a7b73ce3e48f04
Luis Felipe Velasco Lopez <luis.velasco@ucb.edu.bo>
2025-04-15 15:00:54 -0400

    ultimas actualizacion defensa sprint 1 y tests

--- backendSinFront/src/main/java/ucb/com/backendSinFront/Controller/EmpresaController.java
+++ backendSinFront/src/main/java/ucb/com/backendSinFront/Controller/EmpresaController.java
@@ -1,17 +1,16 @@
 package ucb.com.backendSinFront.Controller;
 
-import org.springframework.web.bind.annotation.*;
-import ucb.com.backendSinFront.entity.Empresa;
-import ucb.com.backendSinFront.entity.Usuario;
-import ucb.com.backendSinFront.service.EmpresaService;
-import org.springframework.http.ResponseEntity;
-import org.springframework.http.HttpStatus;
-import ucb.com.backendSinFront.LogHelper;
 import org.springframework.beans.factory.annotation.Autowired;
-import ucb.com.backendSinFront.service.UsuarioService;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import ucb.com.backendSinFront.entity.Empresa;
+import ucb.com.backendSinFront.service.EmpresaService;
 
 import java.util.List;
 import java.util.Optional;
+
 
 @RestController
 @RequestMapping("/api/empresas")
@@ -21,53 +20,43 @@ public class EmpresaController {
   @Autowired
   private EmpresaService empresaService;
 
-  //Get general
+  // Get general
   @GetMapping
-  public List<Empresa> obtenerEmpresa() {
+  public List<Empresa> obtenerEmpresas() {
     return empresaService.obtenerTodas();
   }
 
-  //Get por id
+  // Get por id
   @GetMapping("/{id}")
   public Optional<Empresa> obtenerEmpresa(@PathVariable Long id) {
     return empresaService.obtenerPorId(id);
   }
 
-  //Crear
+  // Crear
   @PostMapping("/create")
   public Empresa crearEmpresa(@RequestBody Empresa empresa) {
     return empresaService.guardar(empresa);
   }
 
-  //Eliminar
+  // Eliminar
   @DeleteMapping("/{id}")
   public void eliminarEmpresa(@PathVariable Long id) {
     empresaService.eliminar(id);
   }
 
-  //Editar
+  // Editar
   @PutMapping("/{id}")
   public Empresa actualizarEmpresa(@PathVariable Long id, @RequestBody Empresa empresaActualizada) {
     return empresaService.actualizarEmpresa(id, empresaActualizada);
   }
 
   @PostMapping("/login")
-  public ResponseEntity<?> autenticarEmpresa(@RequestBody Empresa empresa) {
-    Optional<Empresa> empresaExistente = empresaService.obtenerPorCorreo(empresa.getCorreo());
+  public ResponseEntity<?> loginEmpresa(@RequestBody Empresa empresaLogin) {
+    Optional<Empresa> empresa = empresaService.obtenerPorCorreo(empresaLogin.getCorreo());
 
-    if (empresaExistente.isPresent()) {
-      LogHelper.info(EmpresaController.class, "Empresa encontrada: " + empresaExistente.get().getCorreo());
-      LogHelper.debug(EmpresaController.class, "Contraseña recibida: " + empresa.getPasswordHash());
-      LogHelper.debug(EmpresaController.class, "Contraseña almacenada: " + empresaExistente.get().getPasswordHash());
-
-      if (empresaExistente.get().getPasswordHash().equals(empresa.getPasswordHash())) {
-        return ResponseEntity.ok().body(empresaExistente.get());
-      } else {
-        LogHelper.error(EmpresaController.class, "Contraseña incorrecta para la empresa: " + empresa.getCorreo());
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Credenciales incorrectas");
-      }
+    if (empresa.isPresent() && empresa.get().getPasswordHash().equals(empresaLogin.getPasswordHash())) {
+      return ResponseEntity.ok(empresa.get());
     } else {
-      LogHelper.error(EmpresaController.class, "Empresa no encontrada: " + empresa.getCorreo());
       return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Credenciales incorrectas");
     }
   }
