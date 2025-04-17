e8be417244358033c4a0c58cc2d5b4183f3bdd43
vivi <revollovivian@gmail.com>
2025-04-17 10:11:44 -0330

    Test Usuario1

--- backendSinFront/src/test/java/ucb/com/backendSinFront/Controller/EventoControllerTest.java
+++ backendSinFront/src/test/java/ucb/com/backendSinFront/Controller/EventoControllerTest.java
@@ -1,4 +1,32 @@
+package ucb.com.backendSinFront.Controller;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 class EventoControllerTest {
 
+  @Test
+  void obtenerEventos() {
+
+  }
+
+  @Test
+  void obtenerEventosPorRango() {
+  }
+
+  @Test
+  void obtenerEvento() {
+  }
+
+  @Test
+  void crearEvento() {
+  }
+
+  @Test
+  void eliminarEvento() {
+  }
+
+  @Test
+  void actualizarEvento() {
+  }
 }
