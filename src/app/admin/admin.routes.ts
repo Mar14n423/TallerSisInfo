17abf835dc68f13eef2caa2d17e51c1753411232
Mar14n423 <wara.mercado@ucb.edu.bo>
2025-04-17 22:42:03 -0400

    se agrego el menu para el admin

--- src/app/admin/admin.routes.ts
+++ src/app/admin/admin.routes.ts
@@ -4,17 +4,29 @@ export const adminRoutes: Routes = [
   {
     path: 'admin',
     loadComponent: () =>
-      import('../components/VistaAdmin/homeadmin/homeadmin.component').then(m => m.HomeadminComponent)
-  },
-  {
-    path: 'usuarios',
-    loadComponent: () =>
-      import('../components/VistaAdmin/usuarios/usuarios.component').then(m => m.UsuariosComponent)
-  },
-  {
-    path: 'eventos',
-    loadComponent: () =>
-      import('../components/VistaAdmin/eventos/eventos.component').then(m => m.EventosComponent)
+      import('../components/VistaAdmin/layout/layout.component').then(m => m.LayoutComponent),
+    children: [
+      {
+        path: '',
+        redirectTo: 'dashboard',
+        pathMatch: 'full'
+      },
+      {
+        path: 'dashboard',
+        loadComponent: () =>
+          import('../components/VistaAdmin/homeadmin/homeadmin.component').then(m => m.HomeadminComponent)
+      },
+      {
+        path: 'usuarios',
+        loadComponent: () =>
+          import('../components/VistaAdmin/usuarios/usuarios.component').then(m => m.UsuariosComponent)
+      },
+      {
+        path: 'eventos',
+        loadComponent: () =>
+          import('../components/VistaAdmin/eventos/eventos.component').then(m => m.EventosComponent)
+      }
+    ]
   },
   {
     path: 'login-admin',
