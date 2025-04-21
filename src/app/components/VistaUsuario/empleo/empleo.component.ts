602e02c39665db80f2bc3eedc4cef5b3c729401e
FernandoNB06 <fernando.nogales@ucb.edu.bo>
2025-04-20 21:29:18 -0400

    creacion de carpeta detalle-trabajo mas su logica de navegacion

--- src/app/components/VistaUsuario/empleo/empleo.component.ts
+++ src/app/components/VistaUsuario/empleo/empleo.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { FooterComponent } from '../../../shared/footer/footer.component';
 import { NavbarComponent } from '../../../shared/navbar/navbar.component';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
-
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-empleo',
   standalone: true,
-  imports: [FooterComponent, NavbarComponent, MatProgressBarModule ], // 🔹 Importamos Navbar y Footer
+  imports: [CommonModule, RouterModule,FooterComponent, NavbarComponent, MatProgressBarModule ], // 🔹 Importamos Navbar y Footer
   templateUrl: './empleo.component.html',
   styleUrls: ['./empleo.component.scss']
 })
