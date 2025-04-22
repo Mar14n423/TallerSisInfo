f67edceca608be8ab75f293ec12b66b96f6f0c87
Luis Felipe Velasco Lopez <luis.velasco@ucb.edu.bo>
2025-04-22 14:36:40 -0400

    hicimos para ver mas informacion y recuperando los datos de la BD

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,8 @@ import { Routes } from '@angular/router';
 import { usuarioRoutes } from './usuario/usuario.routes';
 import { adminRoutes } from './admin/admin.routes';
 import { empresaRoutes } from './empresa/empresa.routes';
+import { EmpleoComponent } from './components/VistaUsuario/empleo/empleo.component';
+import { DetalleTrabajoComponent } from './components/VistaUsuario/detalle-trabajo/detalle-trabajo.component';
 
 export const routes: Routes = [
   {
@@ -11,5 +13,13 @@ export const routes: Routes = [
   },
   ...usuarioRoutes,
   ...empresaRoutes,
-  ...adminRoutes
+  ...adminRoutes,
+  {
+    path: 'empleos',
+    component: EmpleoComponent
+  },
+  {
+    path: 'empleos/:id',
+    component: DetalleTrabajoComponent
+  }
 ];
