f4be052727d00537bffed700daaf2663c80106a9
FernandoNB06 <fernando.nogales@ucb.edu.bo>
2025-04-22 02:24:00 -0400

    Agregacion de filtros visuales en la vista empleos

--- src/app/components/VistaUsuario/empleo/empleo.component.ts
+++ src/app/components/VistaUsuario/empleo/empleo.component.ts
@@ -4,12 +4,19 @@ import { NavbarComponent } from '../../../shared/navbar/navbar.component';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-empleo',
   standalone: true,
-  imports: [CommonModule, RouterModule,FooterComponent, NavbarComponent, MatProgressBarModule ], // 🔹 Importamos Navbar y Footer
+  imports: [FormsModule,CommonModule, RouterModule,FooterComponent, NavbarComponent, MatProgressBarModule ], // 🔹 Importamos Navbar y Footer
   templateUrl: './empleo.component.html',
   styleUrls: ['./empleo.component.scss']
 })
-export class EmpleoComponent {}
+export class EmpleoComponent {
+   filtroCategoria: string = '';
+   filtroUbicacion: string = '';
+   filtroEmpresa: string = '';
+   filtroContrato: string = '';   // 🆕 Tipo de contrato (ej: Tiempo completo, Freelance)
+   filtroEstado: string = '';
+ }
