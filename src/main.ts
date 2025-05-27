964a7cbb91bd132e9fa91b87940f1fdf5f810bc9
Fernando Rodriguez <fernando.ale.fars@gmail.com>
2025-05-27 02:52:13 -0400

    Tokens Totalmente funcionales para usuario y administrador

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,14 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { provideRouter } from '@angular/router';
 import { AppComponent } from './app/app.component';
 import { routes } from './app/app.routes';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { authInterceptor } from './app/guards/auth.interceptor';
 
 bootstrapApplication(AppComponent, {
-  providers: [provideRouter(routes),provideHttpClient()]
+  providers: [
+    provideRouter(routes),
+    provideHttpClient(
+      withInterceptors([authInterceptor]) // 👈 Aquí se registra tu interceptor
+    )
+  ]
 }).catch(err => console.error(err));
