f9daa36a3528d7d73750069ca3b9dfce9f89fe1e
Mar14n423 <wara.mercado@ucb.edu.bo>
2025-06-15 15:35:35 -0400

    se mejoro la vista del formulario de registro

--- src/app/components/VistaUsuario/register/register.component.ts
+++ src/app/components/VistaUsuario/register/register.component.ts
@@ -4,7 +4,11 @@ import { FormsModule, ReactiveFormsModule, FormGroup, FormControl, Validators } 
 import { RouterModule } from '@angular/router';
 import { Router } from '@angular/router';
 import { UsuarioService } from './usuario.service';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle'; 
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-register',
@@ -14,7 +18,11 @@ import { MatSlideToggleModule } from '@angular/material/slide-toggle';
     FormsModule, 
     ReactiveFormsModule, 
     RouterModule,
-    MatSlideToggleModule 
+    MatSlideToggleModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule
   ],
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
@@ -34,6 +42,7 @@ export class RegisterComponent {
   constructor(private usuarioService: UsuarioService, private router: Router) {}
 
   toggleColorMode() {
+    
   }
 
   onSubmit() {
